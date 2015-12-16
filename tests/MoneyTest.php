e1c71aac3cb48894b57cb0d818a6838b3a983f2a
Daniel Snook <dsnook@free.fr>
2015-12-16 10:43:28 +0100

    Add Money class & MoneyTest

---
+++ tests/MoneyTest.php
@@ -0,0 +1,20 @@
+<?php
+include "../src/Money.php";
+class MoneyTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testCanBeNegated()
+    {
+        // Arrange
+        $a = new Money(1);
+
+        // Act
+        $b = $a->negate();
+
+        // Assert
+        $this->assertEquals(0, $b->getAmount());
+    }
+
+    // ...
+}
